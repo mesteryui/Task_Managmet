0b68d94b041984fe34b07c2d9c118dcb849d130c
Oscar <oscarodriguez56@gmail.com>
2025-02-15 00:40:17 +0100

    Adding state and task

--- src/main/java/org/example/Task.java
+++ src/main/java/org/example/Task.java
@@ -1,4 +1,11 @@
 package org.example;
 
 public class Task {
+    State state;
+    String task;
+
+    public Task(String state,String task) {
+        this.state = this.state.valueOf(state.toUpperCase());
+        this.task = task;
+    }
 }
