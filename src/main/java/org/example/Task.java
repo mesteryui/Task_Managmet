d590d0720f55f38a267342b2adc843d62b975d42
Oscar <oscarodriguez56@gmail.com>
2025-02-15 00:54:01 +0100

    Documenting task

--- src/main/java/org/example/Task.java
+++ src/main/java/org/example/Task.java
@@ -1,9 +1,19 @@
 package org.example;
 
+/**
+ * Definition of a task with its state and the name of task
+ * @author Oscar Rodriguez
+ * @version 1.0
+ */
 public class Task {
     State state;
     String task;
 
+    /**
+     * Creation of a task
+     * @param state the state of a task
+     * @param task the task itself
+     */
     public Task(String state,String task) {
         this.state = State.valueOf(state.toUpperCase());
         this.task = task;
