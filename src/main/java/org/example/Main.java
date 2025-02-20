1baebbe72495929e4666d05749120bf5797c53bf
Oscar <oscarodriguez56@gmail.com>
2025-02-20 16:34:37 +0100

    Refractoring menu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,17 +2,37 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Task task1 = new Task("todo","ajfalf");
-        Task task2 = new Task("next","ajfalf");
-        Task task3 = new Task("next","alfjd");
-        Task task4 = new Task("done","alfjd");
-        task3.changeState("done");
-        TaskList taskList = new TaskList(task1,task2,task3,task4);
-        taskList.showAllTasks();
+        Scanner sc = new Scanner(System.in);
+        TaskList taskList = new TaskList();
+        int opcion;
+        do {
+            System.out.println("1.Introducir tarea\n2.Ver tareas existentes\n3.Tareas por filtro\n4.Salir");
+            opcion = sc.nextInt();
+            switch (opcion) {
+                case 1:
+                    System.out.println("Introduzca estado de la tarea:");
+                    String estado = sc.next();
+                    System.out.println("Introduzca la tarea:");
+                    String tarea = sc.next();
+                    taskList.addTasks(new Task(estado,tarea));
+                    break;
+                case 2:
+                    taskList.showAllTasks();
+                    break;
+                case 3:
+                    System.out.println("Introduca el estado por el que quiere filtrar");
+                    String stado = sc.next();
+                    taskList.showFilteringTasks(State.valueOf(stado.toUpperCase()));
+            }
+
+
+        } while (opcion!=4);
+        sc.close();
     }
 }
