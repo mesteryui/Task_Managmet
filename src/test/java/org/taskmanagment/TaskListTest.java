ec464f963eb497080459f51bcedc1a8c1988a23e
Oscar <oscarodriguez56@gmail.com>
2025-02-26 15:39:32 +0100

    Añadiendo TaskList y TaskTest

--- src/test/java/org/taskmanagment/TaskListTest.java
+++ src/test/java/org/taskmanagment/TaskListTest.java
@@ -3,7 +3,6 @@ package org.taskmanagment;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class TaskListTest {
--- src/test/java/org/taskmanagment/TaskTest.java
+++ src/test/java/org/taskmanagment/TaskTest.java
@@ -1,7 +1,5 @@
 package org.taskmanagment;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
