4a35a7c61cb84adc5c6aa2409eb8a77ccc5a226a
Oscar <oscarodriguez56@gmail.com>
2025-02-28 09:19:53 +0100

    Chanfing Scanner for Buffered Read because it's more efficient in some issues

--- src/main/java/org/main/Main.java
+++ src/main/java/org/main/Main.java
@@ -3,23 +3,26 @@ package org.main;
 import org.taskmanagment.Task;
 import org.taskmanagment.TaskList;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         TaskList taskList = new TaskList();
         int option;
         do {
             System.out.println("1.Insert Task\n2.View existing tasks\n3.Filtering Tasks\n4.Exit");
-            option = sc.nextInt();
+            option = Integer.parseInt(reader.readLine());
             switch (option) {
                 case 1:
                     System.out.println("Insert the state of Task:");
-                    String state = sc.next();
+                    String state = reader.readLine();
                     System.out.println("Insert the task:");
-                    String task = sc.next();
+                    String task = reader.readLine();
                     taskList.addTasks(new Task(state,task));
                     break;
                 case 2:
@@ -27,13 +30,12 @@ public class Main {
                     break;
                 case 3:
                     System.out.println("Insert the state to filter:");
-                    String stado = sc.next();
+                    String stado = reader.readLine();
                     System.out.println(taskList.showFilteringTasks(stado));
                     break;
             }
 
 
         } while (option!=4);
-        sc.close();
     }
 }
